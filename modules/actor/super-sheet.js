88b465b8f377be6f94a4293862633040e19bfe79
jalensailin <jalen.michalslevy@gmail.com>
2024-02-15 15:46:54 -0500

    fill focus section with bubbles/labels/inputs;

--- modules/actor/super-sheet.js
+++ modules/actor/super-sheet.js
@@ -1,11 +1,11 @@
-/* global ActorSheet mergeObject */
+/* global ActorSheet mergeObject game */
 
 export default class SuperSheet extends ActorSheet {
   /** @override */
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       classes: ["mvrpg", "sheet", "actor"],
-      template: "systems/mvrpg/templates/actor/super-sheet.hbs",
+      template: `systems/${game.system.id}/templates/actor/super-sheet.hbs`,
       width: 600,
       height: 670,
       tabs: [
