15b831704de8a26bad5d830f27887cf0147e72b9
jalensailin <jalen.michalslevy@gmail.com>
2024-03-31 19:24:54 -0400

    fill out settings tab

--- modules/config.js
+++ modules/config.js
@@ -12,6 +12,43 @@ const MVRPG = {
     titanic: "MVRPG.sheets.generic.sizes.titanic",
     gargantuan: "MVRPG.sheets.generic.sizes.gargantuan",
   },
+
+  powerSets: {
+    basic: "MVRPG.sheets.generic.powerSets.basic",
+    elementalControl: "MVRPG.sheets.generic.powerSets.elementalControl",
+    illusion: "MVRPG.sheets.generic.powerSets.illusion",
+    magic: "MVRPG.sheets.generic.powerSets.magic",
+    martialArts: "MVRPG.sheets.generic.powerSets.martialArts",
+    meleeWeapons: "MVRPG.sheets.generic.powerSets.meleeWeapons",
+    omniversalTravel: "MVRPG.sheets.generic.powerSets.omniversalTravel",
+    phasing: "MVRPG.sheets.generic.powerSets.phasing",
+    plasticity: "MVRPG.sheets.generic.powerSets.plasticity",
+    rangedWeapons: "MVRPG.sheets.generic.powerSets.rangedWeapons",
+    resize: "MVRPG.sheets.generic.powerSets.resize",
+    shieldBearer: "MVRPG.sheets.generic.powerSets.shieldBearer",
+    spiderPowers: "MVRPG.sheets.generic.powerSets.spiderPowers",
+    superSpeed: "MVRPG.sheets.generic.powerSets.superSpeed",
+    superStrength: "MVRPG.sheets.generic.powerSets.superStrength",
+    tactics: "MVRPG.sheets.generic.powerSets.tactics",
+    telekenesis: "MVRPG.sheets.generic.powerSets.telekenesis",
+    telepathy: "MVRPG.sheets.generic.powerSets.telepathy",
+    teleportation: "MVRPG.sheets.generic.powerSets.teleportation",
+    weatherControl: "MVRPG.sheets.generic.powerSets.weatherControl",
+  },
+
+  powerDurations: {
+    permanent: "MVRPG.sheets.generic.powerDurations.permanent",
+    instant: "MVRPG.sheets.generic.powerDurations.instant",
+    oneRound: "MVRPG.sheets.generic.powerDurations.oneRound",
+    concentration: "MVRPG.sheets.generic.powerDurations.concentration",
+  },
+
+  powerActions: {
+    none: "MVRPG.sheets.generic.powerActions.none",
+    standard: "MVRPG.sheets.generic.powerActions.standard",
+    movement: "MVRPG.sheets.generic.powerActions.movement",
+    reaction: "MVRPG.sheets.generic.powerActions.reaction",
+  },
 };
 
 export default MVRPG;
--- modules/datamodels/item/power-schema.js
+++ modules/datamodels/item/power-schema.js
@@ -6,6 +6,50 @@ export default class PowerDataModel extends foundry.abstract.TypeDataModel {
   static defineSchema() {
     const { fields } = foundry.data;
     return {
+      powerSets: new fields.ArrayField(
+        new fields.StringField({
+          required: true,
+          nullable: false,
+          initial: "basic",
+          choices: Object.keys(MVRPG.powerSets),
+        }),
+        { required: true, initial: ["basic"] },
+      ),
+      action: new fields.StringField({
+        required: true,
+        nullable: false,
+        initial: "none",
+        choices: Object.keys(MVRPG.powerActions),
+      }),
+      duration: new fields.StringField({
+        required: true,
+        nullable: false,
+        initial: "permanent",
+        choices: Object.keys(MVRPG.powerDurations),
+      }),
+      range: new fields.SchemaField({
+        value: new fields.NumberField({
+          required: true,
+          nullable: false,
+          initial: 0,
+          min: 0,
+        }),
+        multiplyByRank: new fields.BooleanField({
+          required: true,
+          nullable: false,
+          initial: false,
+        }),
+      }),
+      cost: new fields.NumberField({
+        required: true,
+        nullable: false,
+        initial: 0,
+      }),
+      isAttack: new fields.BooleanField({
+        required: true,
+        nullable: false,
+        initial: false,
+      }),
       description: new fields.HTMLField({
         required: true,
         nullable: false,
