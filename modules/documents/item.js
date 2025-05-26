86954590dc569176c67f85eea613644ee4b5e5e4
jalensailin <jalen.michalslevy@gmail.com>
2025-05-26 15:15:22 -0400

    restore drag & drop from sidebar

--- modules/documents/item.js
+++ modules/documents/item.js
@@ -1,25 +1,30 @@
 export default class MVItem extends Item {
-  static async create(data) {
-    if (data.type !== "simpleItem" || data.effects) return super.create(data);
+  /** @inheritdoc */
+  async _preCreate(data, options, user) {
+    if (data.type !== "simpleItem" || data.effects)
+      return super._preCreate(data, options, user);
 
-    data.effects = [];
-    data.effects.push({
-      name: "Damage Multiplier Bonus",
-      img: "icons/skills/wounds/injury-face-impact-orange.webp",
-      changes: [
+    this.updateSource({
+      effects: [
         {
-          key: "system.abilities.melee.damageMultiplierBonus",
-          mode: 2,
-          value: "1",
-        },
-        {
-          key: "system.abilities.agility.damageMultiplierBonus",
-          mode: 2,
-          value: "1",
+          name: "Damage Multiplier Bonus",
+          img: "icons/skills/wounds/injury-face-impact-orange.webp",
+          changes: [
+            {
+              key: "system.abilities.melee.damageMultiplierBonus",
+              mode: 2,
+              value: "1",
+            },
+            {
+              key: "system.abilities.agility.damageMultiplierBonus",
+              mode: 2,
+              value: "1",
+            },
+          ],
         },
       ],
     });
 
-    return super.create(data);
+    return super._preCreate(data, options, user);
   }
 }
